88a9d06112cc1c358b4dbf1aca69f6cd93cdb767
[LK1] <[ssomcandy777@gmail.com]>
2023-08-11 17:51:34 +0900

    Feat: Home 컴포넌트 로그인 페이지로 경로이동

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+// Custom Hooks
+import usePathMove from "@/hooks/usePathMove";
+
+// CSS in JS
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fafafa;
+`;
+
 const Home = () => {
-  return <div>홈 (루트) 페이지 입니다.</div>;
+  const pathMove = usePathMove();
+  return (
+    <Wrapper>
+      <div onClick={() => pathMove("/signup")}>로그인 하기</div>
+    </Wrapper>
+  );
 };
 
 export default Home;
