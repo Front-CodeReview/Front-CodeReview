4d0413f796cd584cd7fa4e21522118ba66ebf357
lk1 <whitecandy777@naver.com>
2023-08-14 01:47:50 +0900

    Feat: 회원가입 기능 추가

--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -1,40 +1,103 @@
+// React
+import React, { useState } from "react";
+
 // Custom Hooks
-import useAuthInput from "@/hooks/useAuthInput";
+import usePathMove from "@/hooks/usePathMove";
 
 // Compoennts
 import Input from "@/common/Input";
 import Button from "@/common/Button";
+import Label from "@/common/Label";
+
+// Util
+import validateInputValue from "@/util/validateInputValue";
 
 const AuthForm = () => {
-  const [inputValue, onChange] = useAuthInput({
+  const pathMove = usePathMove();
+  const [inputValue, setInputValue] = useState({
     name: "",
     email: "",
     password: "",
+    password2: "",
+    tel: "",
   });
+
+  // Input 입력값 State 업데이트
+  const SignUpUserInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setInputValue((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  // 회원가입 Form 전송
+  const SingUpUserSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { name, email, password, tel } = inputValue;
+    const db = {
+      name,
+      email,
+      password,
+      tel,
+    };
+
+    const data = validateInputValue(inputValue);
+
+    if (data) {
+      localStorage.setItem("join", JSON.stringify(db));
+      alert("회원가입이 완료 되었습니다.");
+      pathMove("/");
+    }
+  };
+
   return (
-    <form>
+    <form
+      onSubmit={SingUpUserSubmit}
+      style={{ paddingTop: "100px", maxWidth: "300px", margin: "0 auto" }}
+    >
+      <Label text="닉네임" />
       <Input
         type="text"
         placeholder="닉네임"
         required={true}
         name="name"
-        onChange={onChange}
+        onChange={SignUpUserInfo}
       />
+      <Label text="이메일" />
       <Input
         type="text"
-        placeholder="아이디"
+        placeholder="이메일"
         required={true}
-        name="id"
-        onChange={onChange}
+        name="email"
+        onChange={SignUpUserInfo}
       />
+      <Label text="비밀번호" />
       <Input
         type="password"
         placeholder="비밀번호"
         required={true}
         name="password"
-        onChange={onChange}
+        onChange={SignUpUserInfo}
       />
-      <Button title="회원가입" />
+      <Label text="비밀번호 재확인" />
+      <Input
+        type="password"
+        placeholder="비밀번호 재확인"
+        required={true}
+        name="password2"
+        onChange={SignUpUserInfo}
+      />
+      <Label text="휴대폰 번호" />
+      <Input
+        type="tel"
+        placeholder="휴대폰 번호"
+        required={true}
+        name="tel"
+        onChange={SignUpUserInfo}
+      />
+      <Button type="submit" title="회원가입" />
     </form>
   );
 };
---
+++ src/pages/Auth/SignUp.tsx
@@ -0,0 +1,8 @@
+// Components
+import AuthForm from "@/components/Auth/AuthForm";
+
+const SignUp = () => {
+  return <AuthForm />;
+};
+
+export default SignUp;
