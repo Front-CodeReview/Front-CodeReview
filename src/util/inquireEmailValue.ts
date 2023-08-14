81ac5f60f03bbee379f19e42f24afbd5d3871319
unknown <whitecandy777@naver.com>
2023-08-14 19:57:00 +0900

    Fix: 이메일 중복 검증 에러 수정

--- src/util/inquireEmailValue.ts
+++ src/util/inquireEmailValue.ts
@@ -9,15 +9,11 @@ const inquireEmailValue = (email: string) => {
   const db = localStorage.getItem("join");
   const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email); // 정규 표현식을 사용하여 이메일 형식 검사
   if (!isEmail) return alert("이메일 형식이 올바르지 않습니다.");
-  if (db !== null) {
+  if (db) {
     const dbData = JSON.parse(db);
-    dbData.forEach((data: DBtype) => {
-      if (data.email === email) {
-        return alert("중복된 이메일이 있습니다.");
-      }
-      return alert("사용 가능한 이메일 입니다.");
-    });
+    const isDuplicate = dbData.some((data: DBtype) => data.email === email);
+    if (isDuplicate) return alert("중복된 이메일이 있습니다.");
   }
+  return alert("사용가능한 이메일 입니다.");
 };
-
 export default inquireEmailValue;
